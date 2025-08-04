6bb2c93f5a24822ecfa5467ccbcf741d9783a369
Hemanth Kumar Posa <posahemanth@gmail.com>
2025-08-04 23:00:07 +0530

    Render deploy setup

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -63,10 +63,13 @@ ROOT_URLCONF = 'jobportal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -74,6 +77,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'jobportal.wsgi.application'
 
